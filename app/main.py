5f7d6a95b82b16cae68471a7130fa1faa960c245
allanderek <allan.clark@gmail.com>
2015-06-25 18:47:38 +0100

    Implemented and tested the baron card.

--- app/main.py
+++ app/main.py
@@ -55,6 +55,27 @@ class Game(object):
                 # Nominated player is out of the game
                 self.players.remove(nominated_player)
 
+        if card == Card.baron:
+            if nominated_player is None:
+                raise Exception("You have to nominate a player to play the baron.")
+            if nominated_player not in self.players:
+                raise Exception("You have to baron against a player still in the game.")
+            opponents_card = self.hands[nominated_player]
+            if kept_card > opponents_card:
+                self.players.remove(nominated_player)
+            elif opponents_card > kept_card:
+                # Do nothing, but the current player is out of the game so we
+                # return without placing the current player 
+                return
+            # If the cards are equal nothing happens.
+
+        if card == Card.countess:
+            # This is fine, we need to check above that a player never manages
+            # to avoid discarding the countess when they hold the prince or the
+            # king, but discarding the countess is always fine, but has no
+            # effect.
+            pass
+
         if card == Card.princess:
             # The player is out, so do not append them to the back of the
             # players list.
@@ -88,3 +109,39 @@ class GameTest(unittest.TestCase):
         game.play_turn('a', Card.guard, nominated_player='c', nominated_card=Card.baron)
         self.assertEqual(game.players, ['a'])
 
+    def test_baron(self):
+        """ This simply tests that the baron can knock a player out. We check
+            also that a baron-tie causes no changes. We should of course also
+            check that handmaid makes this card essentially a simple discard.
+        """
+        deck = [ Card.baron, # player a is dealt this card
+                 Card.priest, # player b is dealt this card
+                 Card.baron, # player c is dealt this card
+                 Card.countess, # player d is dealt this card
+                 Card.prince, # a draws prince, higher than b's priest
+                 Card.prince, # c draws prince, lower than d's countess.
+                 ]
+        players = ['a', 'b', 'c', 'd']
+        game = Game(players, deck=deck)
+        game.play_turn('a', Card.baron, nominated_player='b')
+        game.draw_card()
+        game.play_turn('c', Card.baron, nominated_player='d')
+        self.assertEqual(game.players, ['d', 'a'])
+        deck = [ Card.baron, # player a is dealt this card
+                 Card.priest, # player b is dealt this card
+                 Card.baron, # player c is dealt this card
+                 Card.prince, # player d is dealt this card
+                 Card.prince, # a draws prince, higher than b's priest,
+                              # but the same as d's prince.
+                 ]
+        players = ['a', 'b', 'c', 'd']
+        game = Game(players, deck=deck)
+        game.play_turn('a', Card.baron, nominated_player='d')
+        # Both a and d still in the game due to the drawn baron showdown.
+        self.assertEqual(game.players, ['b', 'c', 'd', 'a'])
+
+    def test_prince(self):
+        """ This tests the prince has the desired effect, we will also check
+            that you can prince yourself.
+        """
+        pass
